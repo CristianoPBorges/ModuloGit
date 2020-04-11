e3e0a084fe8b8a28d2ec4a989ca5a469dc8329a0
Cristiano Borges <cris_tecno@htomail.com>
2020-04-11 13:52:09 +0100

    Alterei o arquivo index e tbm o style

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
-<html>
-<head>
-        <title> Site Alcateia Moto Clube</title>
-</Head>
-<body>
-  
-</body>
+<html lang="pt-br" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"
+      xmlns="http://www.w3.org/1999/html">
+    <head>
+    <meta charset="UTF-8">
+    <title>Alcateia Moto Clube</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    </head>
+    <body>
+        <div id=topo>
+            <h1>Alcateia Moto Clube </h1>
+            <h3> Conduta & Atitude </h3>
+        </div>
+        // criando um pequeno comentario
+        <div id="corpo">
+            teste para o GitHub
+        </div>
+    </body>
 </html>
